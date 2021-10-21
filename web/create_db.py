0041d69755db1bbb07f167b5e376efd7d3bc2579
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-10-21 15:13:49 -0500

    feat: add sql models

--- web/create_db.py
+++ web/create_db.py
@@ -1,5 +1,38 @@
-# create_db.py
 
-from app import db
+from enum import auto
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship  # for configuration
+from sqlalchemy import create_engine
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.schema import Column
+# create declarative_base instance
+from sqlalchemy.sql.sqltypes import Integer, String
+from .app import engine
+# we'll add classes here#creates a create_engine instance at the bottom of the file
+Base = declarative_base()
 
-db.create_all()
+Base.metadata.create_all(engine)
+
+
+class Medico(Base):
+    __tablename__ = 'medico'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    nombre = Column(String, nullable=False)
+    apellido = Column(String, nullable=False)
+    direccion = Column(String)
+    telefono = Column(String)
+    email = Column(String)
+    especialidad = Column(String)
+
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    nombre = Column(String, nullable=False)
+    apellido = Column(String, nullable=False)
+    direccion = Column(String)
+    telefono = Column(String)
+    email = Column(String)
+    especialidad = Column(String)
